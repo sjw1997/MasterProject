33f9cc4ed86631ded752eb4815c8a9844e6e39a1
sjw <sjww1997@126.com>
2020-09-02 17:29:45 +0800

    Fix bugs in IndexMinPQ

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
         In in = new In("E:\\learning\\graduate\\detnet\\Code\\data\\tinyDirectedGraph.txt");
         Graph G = new DirectedGraph(in);
-        ShortestPath sp = new ShortestPath(G, 0);
+        ShortestPath sp = new ShortestPath(G, 5);
         for (int v = 0; v < G.V(); v++) {
-            System.out.printf("%d->%d %.2f\n", 0, v, sp.distTo(v));
+            System.out.printf("%d->%d %.2f\n", 5, v, sp.distTo(v));
         }
     }
 }
